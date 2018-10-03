1c37eb64e14feb576890efad27a0dc322514f09e
Avanesh Sharma <Avanesh.Sharma@rsystems.com>
2018-10-03 14:57:15 +0530

    String Boot Hello World Example

---
+++ SpringBootHelloWorld/src/main/java/com/sharma/hello/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.sharma.hello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	@RequestMapping("/")
+	public String helloWorld() {
+		
+		return "Hello World ! ! ! ! ";
+	}
+	
+}
